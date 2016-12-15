a5083df98d0ba3572b2c426aa5f92d3a5fcb6ae2
weifei <hanfei@firewinggames.com>
2016-12-15 14:18:59 +0800

    Add class

--- SpiderNHDZ.py
+++ SpiderNHDZ.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding:utf-8 -*-
 import urllib
 import urllib2
@@ -10,28 +11,28 @@ sys.setdefaultencoding('utf8')
 page = 2
 url = 'http://www.qiushibaike.com/hot/page/' + str(page)
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = { 'User-Agent' : user_agent }
+headers = {'User-Agent': user_agent}
 try:
-    request = urllib2.Request(url,headers = headers)
+    request = urllib2.Request(url, headers=headers)
     response = urllib2.urlopen(request)
     content = response.read().decode('utf-8')
-    pattern = re.compile('<div.*?class="author.*?>.*?<a.*?</a>.*?<a.*?>.*?<h2>(.*?)<.*?>.*?</a>.*?<div.*?class'+
-    					'="content.*?<span>(.*?)</span>.*?<div.*?class="main-text.*?>(.*?)<div',re.S)
-   	#将</br>替换为\t
-    replaceBr= re.compile('<br/>')
+    pattern = re.compile('<div.*?class="author.*?>.*?<a.*?</a>.*?<a.*?>.*?<h2>(.*?)<.*?>.*?</a>.*?<div.*?class' +
+                         '="content.*?<span>(.*?)</span>.*?<div.*?class="main-text.*?>(.*?)<div', re.S)
+    # 将</br>替换为\t
+    replaceBr = re.compile('<br/>')
 
-    items = re.findall(pattern,content)
+    items = re.findall(pattern, content)
 
     for item in items:
-        #haveImg = re.search("img",item[3])
-        #if not haveImg:
-            print '昵称 ' + item[0]
-            str = item[1];
-            str = re.sub(replaceBr,"\n",str)
-            print '内容:\n' + str#item[1]
-            print '神评论 ' + item[2]
+        # haveImg = re.search("img",item[3])
+        # if not haveImg:
+        print '昵称 ' + item[0]
+        str = item[1]
+        str = re.sub(replaceBr, "\n", str)
+        print '内容:\n' + str  # item[1]
+        print '神评论 ' + item[2]
 except urllib2.URLError, e:
-    if hasattr(e,"code"):
+    if hasattr(e, "code"):
         print e.code
-    if hasattr(e,"reason"):
-        print e.reason  
+    if hasattr(e, "reason"):
+        print e.reason
